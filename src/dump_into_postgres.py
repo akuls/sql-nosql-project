a953dc4b95623c40b9cded151a65ea6048bf3b67
Akul <akul911@gmail.com>
2017-03-29 15:12:45 -0400

    Removed all data files

--- src/dump_into_postgres.py
+++ src/dump_into_postgres.py
@@ -145,25 +145,27 @@ def populate_data(conn):
 		song = pickle.load(open(DATA+song_name, "r"))
 
 		song = fix_format(song)
+		print song
+		break
 
-		#Artist identity
-		artist_dict["INSERT into artist_identity VALUES(" + song['artist_id']+ "," + song['artist_playmeid'] + "," + song['artist_7digitalid'] + "," + song['artist_mbid'] + ")"] = song_name
+	# 	#Artist identity
+	# 	artist_dict["INSERT into artist_identity VALUES(" + song['artist_id']+ "," + song['artist_playmeid'] + "," + song['artist_7digitalid'] + "," + song['artist_mbid'] + ")"] = song_name
 		
-		#Track identity
-		track_identity_set.add("INSERT into track_identity VALUES(" + song['track_id']+ "," + song['song_id'] + "," + song['audio_md5'] + "," + song['release_7digitalid'] + "," + song['track_7digitalid'] + ")")
+	# 	#Track identity
+	# 	track_identity_set.add("INSERT into track_identity VALUES(" + song['track_id']+ "," + song['song_id'] + "," + song['audio_md5'] + "," + song['release_7digitalid'] + "," + song['track_7digitalid'] + ")")
 		
-		#Track basic info
-		track_basic_info_set.add("INSERT into track_basic_info VALUES(" + song['track_id']+ "," + song['song_id'] + "," + song['release'] + "," + song['title'] + "," + song['duration'] + "," + song['year'] + ")")
+	# 	#Track basic info
+	# 	track_basic_info_set.add("INSERT into track_basic_info VALUES(" + song['track_id']+ "," + song['song_id'] + "," + song['release'] + "," + song['title'] + "," + song['duration'] + "," + song['year'] + ")")
 
-		#Track technical info
-		track_tech_info_set.add("INSERT into track_tech_info VALUES(" + song['track_id']+ "," + song['song_id'] + "," + song['song_hotttnesss'] + "," + song['danceability'] + "," + song['start_of_fade_out'] + "," + song['end_of_fade_in'] + \
-			"," + song['energy'] + "," + song['key'] + "," + song['key_confidence'] + "," + song['loudness'] + "," + song['mode'] + "," + song['mode_confidence'] + "," + song['tempo'] + "," + song['time_signature'] + \
-			"," + song['time_signature_confidence'] + "," + song['analysis_sample_rate'] + ")")
+	# 	#Track technical info
+	# 	track_tech_info_set.add("INSERT into track_tech_info VALUES(" + song['track_id']+ "," + song['song_id'] + "," + song['song_hotttnesss'] + "," + song['danceability'] + "," + song['start_of_fade_out'] + "," + song['end_of_fade_in'] + \
+	# 		"," + song['energy'] + "," + song['key'] + "," + song['key_confidence'] + "," + song['loudness'] + "," + song['mode'] + "," + song['mode_confidence'] + "," + song['tempo'] + "," + song['time_signature'] + \
+	# 		"," + song['time_signature_confidence'] + "," + song['analysis_sample_rate'] + ")")
 
-		#Sang
-		sang_set.add("INSERT into sang VALUES(" + song['artist_id']+ "," + song['artist_mbid'] + "," + song['track_id'] + "," + song['song_id'] + ")")
+	# 	#Sang
+	# 	sang_set.add("INSERT into sang VALUES(" + song['artist_id']+ "," + song['artist_mbid'] + "," + song['track_id'] + "," + song['song_id'] + ")")
 
-	insert_into_all_tables(conn, artist_dict, track_identity_set, track_basic_info_set, track_tech_info_set, sang_set)
+	# insert_into_all_tables(conn, artist_dict, track_identity_set, track_basic_info_set, track_tech_info_set, sang_set)
 	pass
 
 def build_tables_and_populate_data():
@@ -178,7 +180,7 @@ def build_tables_and_populate_data():
 		else:
 			print "Connected to DB successfully!"
 
-			build_tables(conn)
+			#build_tables(conn)
 			print 'All tables created!'
 
 			populate_data(conn)
